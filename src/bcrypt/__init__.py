42c1f36fb87630d9a05d7257080d67527816eceb
Jonas Michaelis <jonas.michaelis@outlook.com>
2021-05-30 01:13:08 +0200

    ignore \x00

--- src/bcrypt/__init__.py
+++ src/bcrypt/__init__.py
@@ -77,8 +77,8 @@ def hashpw(password: bytes, salt: bytes) -> bytes:
     if isinstance(password, str) or isinstance(salt, str):
         raise TypeError("Strings must be encoded before hashing")
 
-    if b"\x00" in password:
-        raise ValueError("password may not contain NUL bytes")
+    #if b"\x00" in password:
+    #    raise ValueError("password may not contain NUL bytes")
 
     # bcrypt originally suffered from a wraparound bug:
     # http://www.openwall.com/lists/oss-security/2012/01/02/4
@@ -115,10 +115,10 @@ def checkpw(password: bytes, hashed_password: bytes) -> bool:
     if isinstance(password, str) or isinstance(hashed_password, str):
         raise TypeError("Strings must be encoded before checking")
 
-    if b"\x00" in password or b"\x00" in hashed_password:
-        raise ValueError(
-            "password and hashed_password may not contain NUL bytes"
-        )
+    #if b"\x00" in password or b"\x00" in hashed_password:
+    #    raise ValueError(
+    #        "password and hashed_password may not contain NUL bytes"
+    #    )
 
     ret = hashpw(password, hashed_password)
 
